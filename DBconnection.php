efc3f9b078054809883976e65e700eaacc66df57
curryl34v3s <44476245+curryl34v3s@users.noreply.github.com>
2018-11-22 16:03:27 +0530

    Create DBconnection.php

---
+++ DBconnection.php
@@ -0,0 +1,35 @@
+
+//DBConnection.php
+<?php
+//Establishing connection with the database
+define('DB_SERVER', 'localhost:3306');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root123');
+define('DB_DATABASE', 'books'); //where books is the database name
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
+<br>
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+//DBConnection.php
+ 
+<?php
+//Establishing connection with the database
+define('DB_SERVER', 'localhost:3306');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root123');
+define('DB_DATABASE', 'books'); //where books is the database name
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+ 
+?>
+<br>
